dc9d7b076c00117e5d7b08a0dd0edc3913bce530
Ali Habibian <ali.habibian04@gmail.com>
2021-11-20 01:55:46 +0330

    Mapping Users Class Manually and Create JPA Configuration File

---
+++ src/test/java/com/bookstore/entity/UsersTest.java
@@ -0,0 +1,37 @@
+package com.bookstore.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersTest {
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    @BeforeEach
+    void initial() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("BookStoreWebsite");
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @Test
+    void test_for_persist_user() {
+        Users user = new Users();
+        user.setFullName("Bahram Mahdi");
+        user.setEmail("Bahram@mail.com");
+        user.setPassword("12345");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(user);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        entityManagerFactory.close();
+
+        System.out.println("User added");
+    }
+}
